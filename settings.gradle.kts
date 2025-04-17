30915ae063c8ebd7f59b8401ddb1cd8fe1115eba
crow <1424630050@qq.com>
2025-04-17 17:03:17 +0800

    :tada: sync project

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "KuiklyDemo"
+include(":androidApp")
+include(":shared")
