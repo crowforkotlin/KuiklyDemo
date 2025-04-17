2efd66274c902842ce1eaddcd8faa3ad1ef68201
crow <1424630050@qq.com>
2025-04-17 17:23:09 +0800

    :arrow_up: Update Kotlin 2.0.21 :arrow_up: Update Kuikly 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("7.4.2").apply(false)
     id("com.android.library").version("7.4.2").apply(false)
-    kotlin("android").version("1.9.22").apply(false)
-    kotlin("multiplatform").version("1.9.22").apply(false)
-    id("com.google.devtools.ksp").version("1.9.22-1.0.16").apply(false)
+    kotlin("android").version("2.0.21").apply(false)
+    kotlin("multiplatform").version("2.0.21").apply(false)
+    id("com.google.devtools.ksp").version("2.0.21-1.0.28").apply(false)
 }
